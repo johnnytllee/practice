e03bd3d706ba34601bb4759803e1966341983929
johnny.lee <johnny@ASDTWS2>
2013-07-02 09:27:34 +0800

    Use of environ and getenv().

--- linux_programming/printenv.c
+++ linux_programming/printenv.c
@@ -1,4 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+extern char **environ;
+
+void use_env(char *env[])
+{
+	int i = 0;
+	printf("\n\nVia char *env[]\n");
+	while (env[i]) {
+		printf("[%p] -> [%p] %s\n",&env[i],env[i],env[i]);
+		i++;
+	}
+}
+
+void use_environ(void)
+{
+	char **p = environ;
+	printf("\n\nVia extern char **environ;\n");
+	while (*p) {
+		printf("[%p] -> [%p] %s\n",p,*p,*p);
+		p++;
+	}
+}
+
+void use_getenv(const char *env_name)
+{
+	char *env = getenv(env_name);
+	printf("%s=%s\n",env_name,env?env:"");
+}
 
 int main(int argc, char *argv[], char *env[])
 {
@@ -6,7 +35,7 @@ int main(int argc, char *argv[], char *env[])
 	
 	int i;
 
-	printf("&argc = [%p] %p\n",&argc,*(&argc));
+	printf("&argc = [%p] %d\n",&argc,*(&argc));
 	printf("&argv = [%p] %p\n",&argv,*(&argv));
 	printf("&env  = [%p] %p\n",&env,*(&env));
 
@@ -24,11 +53,13 @@ int main(int argc, char *argv[], char *env[])
 		i++;
 	}
 #endif
-	i = 0;
-	while (env[i]) {
-		printf("[%p] -> [%p] %s\n",&env[i],env[i],env[i]);
-		i++;
-	}
+	use_env(env);
+	use_environ();
+
+	printf("\n\nDemo getenv\n");
+	use_getenv("PWD");
+	use_getenv("");
+	use_getenv("SHIT");
 
 	return(0);
 }
